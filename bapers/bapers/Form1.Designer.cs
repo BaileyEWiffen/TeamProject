9bb93e3a47d67b6a1d9c37a864fe2fb1673e54d5
Bailey-city <72195685+Bailey-city@users.noreply.github.com>
2021-03-08 14:00:17 +0000

    Connected to database

--- bapers/bapers/Form1.Designer.cs
+++ bapers/bapers/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LB_dbCheck = new System.Windows.Forms.Label();
+            this.BT_enter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LB_dbCheck
+            // 
+            this.LB_dbCheck.AutoSize = true;
+            this.LB_dbCheck.Location = new System.Drawing.Point(483, 315);
+            this.LB_dbCheck.Name = "LB_dbCheck";
+            this.LB_dbCheck.Size = new System.Drawing.Size(35, 13);
+            this.LB_dbCheck.TabIndex = 0;
+            this.LB_dbCheck.Text = "label1";
+            // 
+            // BT_enter
+            // 
+            this.BT_enter.Location = new System.Drawing.Point(270, 315);
+            this.BT_enter.Name = "BT_enter";
+            this.BT_enter.Size = new System.Drawing.Size(125, 40);
+            this.BT_enter.TabIndex = 1;
+            this.BT_enter.Text = "button1";
+            this.BT_enter.UseVisualStyleBackColor = true;
+            this.BT_enter.Click += new System.EventHandler(this.BT_enter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BT_enter);
+            this.Controls.Add(this.LB_dbCheck);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LB_dbCheck;
+        private System.Windows.Forms.Button BT_enter;
     }
 }
 
--- bapers/bapers/Form1.cs
+++ bapers/bapers/Form1.cs
@@ -1,20 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace bapers
 {
+    
     public partial class Form1 : Form
     {
+       
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
         public Form1()
         {
             InitializeComponent();
+           
+
+
+        }
+
+        private void BT_enter_Click(object sender, EventArgs e)
+        {
+            server = "localhost";
+            database = "bapers";
+            uid = "root";
+            password = "network";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            connection.Open();
+            MessageBox.Show("Connection Open  !");
+            connection.Close();
+
+
         }
     }
 }
