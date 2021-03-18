c6980d7e3e35be7d0a99a9b65a6c01d8de4e8c71
FinlayQ <80168232+FinlayQ@users.noreply.github.com>
2021-03-18 14:28:51 +0000

    custcreation

--- bapers/bapers/F_CustCreation.cs
+++ bapers/bapers/F_CustCreation.cs
@@ -7,19 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace bapers
 {
     public partial class F_CustCreation : Form
+
+
     {
+        private string server = "localhost";
+        private string database = "bapers";
+        private string uid = "root";
+        private string pw = "network";
+
         public F_CustCreation()
         {
             InitializeComponent();
         }
 
+
         private void F_CustCreation_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtFullName.Text == "" || txtConName.Text == "" || txtAddress.Text == "" || txtPhoneNum.Text == "")//checks all boxes are filled
+            {
+                MessageBox.Show("One or more fields not completed", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                string connectionString = "SERVER=" + server + "; DATABASE=" + database + "; UID=" + uid + "; PASSWORD=" + pw + ";";
+                SqlConnection connection = new SqlConnection(connectionString);//opens connection
+                SqlCommand cmd = new SqlCommand("sp_insert", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@customer name", txtFullName.Text);
+                cmd.Parameters.AddWithValue("@contact name", txtConName.Text);
+                cmd.Parameters.AddWithValue("@address", txtAddress.Text);
+                cmd.Parameters.AddWithValue("@phone", txtPhoneNum.Text);
+                connection.Open();
+                int i = cmd.ExecuteNonQuery();
+
+                connection.Close();
+                MessageBox.Show("Customer Successfully Created", "Customer Created", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+
+
+            }
+        }
     }
 }
+
